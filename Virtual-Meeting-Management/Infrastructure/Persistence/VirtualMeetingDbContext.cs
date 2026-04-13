af5ad597a2881477855cdb575652f9eaf743a803
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-04-13 20:58:48 +0200

    Create DbContext class

---
+++ Virtual-Meeting-Management/Infrastructure/Persistence/VirtualMeetingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class VirtualMeetingDbContext : IdentityDbContext
+    {
+        public VirtualMeetingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected virtual void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
