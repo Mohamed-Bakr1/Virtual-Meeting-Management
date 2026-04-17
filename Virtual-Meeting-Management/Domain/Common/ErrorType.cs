10158e9a5bcc221517b8fbdc4ff1cf7475e521ab
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-04-17 18:06:40 +0200

    Create ErrorType

---
+++ Virtual-Meeting-Management/Domain/Common/ErrorType.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Common
+{
+    public enum ErrorType
+    {
+        Validation,             // 400 Bad Request
+        NotFound,               // 404 Not Found
+        Unauthorized,           // 401 Unauthorized
+        Forbidden,              // 403 Forbidden
+        Conflict,               // 409 Conflict
+        ServerError,            // 500 Internal Server Error
+        ServiceUnavailable,     // 503 Service Unavailable
+    }
+}
