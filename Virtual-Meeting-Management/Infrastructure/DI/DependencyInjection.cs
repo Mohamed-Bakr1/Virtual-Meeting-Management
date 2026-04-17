7a709fde980ff9007544e1280711a5b80c9992ca
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-04-17 17:08:07 +0200

    Create DependencyInjection

---
+++ Virtual-Meeting-Management/Infrastructure/DI/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<VirtualMeetingDbContext>(options =>
+                options.UseMySql(
+                    configuration.GetConnectionString("ConnectionStrings"),
+                    ServerVersion.AutoDetect(configuration.GetConnectionString("ConnectionStrings"))
+                )
+            );
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            return services;
+        }
+    }
+}
