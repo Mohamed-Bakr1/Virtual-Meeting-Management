e7d5317df1bdc586485174ac5046bfca6429da6d
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-04-17 15:45:38 +0200

    Create IRepository

---
+++ Virtual-Meeting-Management/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity?> GetByIdAsync(object id);
+        Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRange(IEnumerable<TEntity> entities);
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
+        Task DeleteAsync(object id);
+        void Delete(TEntity entity);
+        void DeleteRange(IEnumerable<TEntity> entities);
+        Task<int> SaveChangesAsync();
+    }
+}
