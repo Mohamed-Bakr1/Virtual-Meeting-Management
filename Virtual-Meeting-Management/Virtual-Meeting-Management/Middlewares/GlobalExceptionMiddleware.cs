3b581aab8906e88fb271a45a7f9734198035ee00
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-04-18 15:13:22 +0200

    Create GlobalExceptionMiddleware

---
+++ Virtual-Meeting-Management/Virtual-Meeting-Management/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿namespace Virtual_Meeting_Management.Middlewares
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+        public GlobalExceptionMiddleware(RequestDelegate next,
+                                         ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        //We can Enhance it more (Talk with GPT about that)
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var response = new
+                {
+                    success = false,
+                    message = "Internal Server Error"
+                };
+
+                await context.Response.WriteAsJsonAsync(response);
+            }
+        }
+    }
+}
