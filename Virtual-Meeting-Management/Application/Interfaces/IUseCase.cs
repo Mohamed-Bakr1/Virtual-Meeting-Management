c1ddeecd77025a7c091c8ca81639fb1dcbc800a2
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-17 11:34:04 +0300

    Create IUseCase

---
+++ Virtual-Meeting-Management/Application/Interfaces/IUseCase.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Interfaces
+{
+    public interface IUseCase<TResult>
+    {
+        Task<TResult> ExecuteAsync();
+    }
+    public interface IUseCase<TInput, TResult>
+    {
+        Task<TResult> ExecuteAsync(TInput input);
+    }
+    public interface IUseCase<Tinput1, Tinput2, TResult>
+    {
+        Task<TResult> ExecuteAsync(Tinput1 input1, Tinput2 input2);
+    }
+}
