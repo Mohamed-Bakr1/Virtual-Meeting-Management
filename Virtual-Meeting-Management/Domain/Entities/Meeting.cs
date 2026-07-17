04ad46e247dd6cf2643de4da2535621b9e3a474c
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-17 11:35:05 +0300

    Create Meeting model

---
+++ Virtual-Meeting-Management/Domain/Entities/Meeting.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class Meeting
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string HostId { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
