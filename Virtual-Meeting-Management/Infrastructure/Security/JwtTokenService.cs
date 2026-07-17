5a77604026d657ec16c8be22c45a801d1ea48fa1
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-17 11:37:39 +0300

    Create JwtTokenService

---
+++ Virtual-Meeting-Management/Infrastructure/Security/JwtTokenService.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces.IUser;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Security
+{
+    public class JwtTokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(IList<Claim> claims)
+        {
+            var secretKey = _configuration.GetSection("SecretKey").Value;
+
+            var secretKeyByte = Encoding.UTF8.GetBytes(secretKey);
+
+            SecurityKey securityKey = new SymmetricSecurityKey(secretKeyByte);
+
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var expire = DateTime.UtcNow.AddDays(30);
+
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(claims: claims, expires: expire, signingCredentials: signingCredentials);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            var token = handler.WriteToken(jwtSecurityToken);
+            return token;
+        }
+    }
+}
