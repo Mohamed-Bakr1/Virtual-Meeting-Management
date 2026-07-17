b0c35c1de454ebcf89d48e0e792e322955933550
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-17 11:37:17 +0300

    Switch to InfrastructureUser; add Meeting entity config
    - Changed Identity user type to InfrastructureUser in DbContext.
    - Added DbSet<Meeting> and configured Meeting entity:
      - HostId is required, max length 450, and indexed.
      - No foreign key constraint for HostId.
    - Seeded default "User" IdentityRole.

--- Virtual-Meeting-Management/Infrastructure/Persistence/VirtualMeetingDbContext.cs
+++ Virtual-Meeting-Management/Infrastructure/Persistence/VirtualMeetingDbContext.cs
@@ -1,18 +1,46 @@
-﻿using Infrastructure.Identity;
+﻿using Domain.Entities;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence
 {
-    public class VirtualMeetingDbContext : IdentityDbContext<ApplicationUser>
+    public class VirtualMeetingDbContext : IdentityDbContext<InfrastructureUser>
     {
         public VirtualMeetingDbContext(DbContextOptions<VirtualMeetingDbContext> options) : base(options)
         {
         }
 
-        protected virtual void OnModelCreating(ModelBuilder builder)
+        public DbSet<Meeting> Meetings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+
+            builder.Entity<Meeting>(entity =>
+            {
+                // Configure HostId as a standard column. 
+                // Notice there is NO .HasOne() or .HasForeignKey() relationship mapping!
+                entity.Property(m => m.HostId)
+                      .IsRequired()
+                      .HasMaxLength(450); // Note: ASP.NET Identity string IDs are 450 characters by default.
+
+                // 💡 PRO-TIP: Add an index on HostId. 
+                // Because you don't have a Foreign Key, the database won't automatically index this column.
+                // You will likely query "Get all meetings for this user", so an index is highly recommended for performance.
+                entity.HasIndex(m => m.HostId);
+            });
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            );
         }
     }
 }
