2d3e93727354e57f7f87b9afab1c8e5fb1cbf903
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-17 11:34:14 +0300

    Create RegisterUseCase

---
+++ Virtual-Meeting-Management/Application/UseCases/RegisterUseCase.cs
@@ -0,0 +1,31 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Application.Interfaces.IUser;
+using Domain.Common;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.UseCases
+{
+    public class RegisterUseCase : IUseCase<RegisterDto, Result>
+    {
+        private readonly IAccountService _accountService;
+
+        public RegisterUseCase(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public async Task<Result> ExecuteAsync(RegisterDto registerDto)
+        {
+            var CheckEmailExistence = await _accountService.CheckEmailExistenceAsync(registerDto.Email);
+
+            if (!CheckEmailExistence.IsSuccess)
+                return CheckEmailExistence;
+
+            var result = await _accountService.RegisterAsync(registerDto);
+
+            return result;
+        }
+    }
+}
