7116d68e7652451e818e017893e9056d2e36d505
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-17 11:34:50 +0300

    Create ApplicationLayerRegistration

---
+++ Virtual-Meeting-Management/Application/ApplicationLayerRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Application.UseCases;
+using Domain.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ApplicationLayerRegistration
+    {
+        public static IServiceCollection AddApplicationLayer(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddScoped<IUseCase<RegisterDto, Result>, RegisterUseCase>();
+
+            return services;
+        }
+    }
+}
