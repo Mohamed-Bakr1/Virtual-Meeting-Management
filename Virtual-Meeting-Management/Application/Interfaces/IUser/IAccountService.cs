a82d2f2a5712bda0d2ecd0c58799ad978d2017f2
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-17 11:33:46 +0300

    Add IAccountService interface for user account management
    Introduced the IAccountService interface in the Application.Interfaces.IUser namespace. This interface provides async methods for user registration and email existence checks, with placeholders for additional account-related operations. Added necessary using directives for DTOs and domain results.

---
+++ Virtual-Meeting-Management/Application/Interfaces/IUser/IAccountService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs;
+using Domain.Common;
+
+namespace Application.Interfaces.IUser
+{
+    public interface IAccountService
+    {
+        //Task<Result<ValidloginDto>> LoginAsync(ApplicationUser user);
+        Task<Result<string>> RegisterAsync(RegisterDto registerDto);
+        Task<Result> CheckEmailExistenceAsync(string email);
+        //Task<Result<ApplicationUser>> CheckEmailExistenceWithReturnAsync(string email);
+        //Task<Result> CheckPasswordMatchAsync(ApplicationUser user, string password);
+        //Task<Result<ExternalLoginClaimsDTO>> ValidateAndExtractExternalLoginClaims(ExternalLoginDto externalLoginDto, ClaimsPrincipal externalLoginClaims);
+        //Task<ValidloginDto> PrepareLoginResponseAsync(ApplicationUser user);
+        //Task<Result<ValidloginDto>> StoreExternalLoginAsync(string providerKey, string provider, ApplicationUser user);
+        //Task<Result> StoreUserBasedOnRoleAsync(string userID, string role, string EducationLevelOrSubject);
+        //Task<Result<UserLoginDataDto>> VerifyOtpAsync(VerifyOTPDto verifyOTPDto);
+        //Task<Result> ForgetPasswordAsync(ApplicationUser user, string newPassword);
+    }
+}
