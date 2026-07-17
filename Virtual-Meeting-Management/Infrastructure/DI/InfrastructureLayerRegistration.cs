9952248fc00871a94988ef989765fc1322bb4a5e
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-17 11:36:11 +0300

    Add infrastructure DI registration and config class
    Introduced the DependencyInjection static class in Infrastructure.DI, providing the AddInfrastructureLayer extension method for centralized service registration. This sets up the DbContext with MySQL, registers repositories and services, and configures relaxed Identity password requirements. Centralizes infrastructure dependency injection setup for the application.

---
+++ Virtual-Meeting-Management/Infrastructure/DI/InfrastructureLayerRegistration.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces.IUser;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Infrastructure.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureLayer(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<VirtualMeetingDbContext>(options =>
+                options.UseMySql(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    ServerVersion.AutoDetect(configuration.GetConnectionString("DefaultConnection"))
+                )
+            );
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<ITokenService, JwtTokenService>();
+
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            });
+
+            return services;
+        }
+    }
+}
