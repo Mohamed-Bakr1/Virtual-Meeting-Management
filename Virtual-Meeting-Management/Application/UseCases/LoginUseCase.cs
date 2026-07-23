4d12d3803d030564ce332a5a4fa82c0bb9e37cde
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-23 19:38:22 +0300

    Craete LoginUseCase

---
+++ Virtual-Meeting-Management/Application/UseCases/LoginUseCase.cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Application.Interfaces.IUser;
+using Domain.Common;
+
+namespace Application.UseCases
+{
+    public class LoginUseCase : IUseCase<LoginDto, Result<ValidLoginDto>>
+    {
+        private readonly IAccountService _accountService;
+
+        public LoginUseCase(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public async Task<Result<ValidLoginDto>> ExecuteAsync(LoginDto loginDto)
+        {
+            var result = await _accountService.LoginAsync(loginDto);
+            return result;
+        }
+    }
+}
