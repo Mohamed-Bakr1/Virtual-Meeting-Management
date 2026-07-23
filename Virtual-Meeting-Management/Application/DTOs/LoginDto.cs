e1060c5da9073bb8edfa9e5448ce7514dc77d427
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-23 19:37:39 +0300

    Create LoginDto

---
+++ Virtual-Meeting-Management/Application/DTOs/LoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address!")]
+        [DefaultValue("cabtinmohamed32@gmail.com")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The Password field is required.")]
+        [DefaultValue("123456")]
+        public string Password { get; set; }
+    }
+}
