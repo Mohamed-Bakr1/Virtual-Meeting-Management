1f34853c215648c3fef8da6036d76e47f8877e6f
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-23 19:38:10 +0300

    Create ValidLoginDto

---
+++ Virtual-Meeting-Management/Application/DTOs/ValidLoginDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs
+{
+    public class ValidLoginDto
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
