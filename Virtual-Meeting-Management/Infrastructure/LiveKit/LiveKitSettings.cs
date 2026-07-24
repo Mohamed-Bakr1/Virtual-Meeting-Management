09913c92051c94ca19c43920f8b45de7b0f8445e
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-24 18:46:36 +0300

    Create LiveKitSetting

---
+++ Virtual-Meeting-Management/Infrastructure/LiveKit/LiveKitSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.LiveKit
+{
+    public class LiveKitSettings
+    {
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+        public string Url { get; set; }
+    }
+}
