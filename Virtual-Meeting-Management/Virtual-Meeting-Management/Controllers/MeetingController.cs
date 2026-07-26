ce1dcd60934dd4acf157fefd277c8202ea0926d8
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-26 21:59:38 +0300

    Create MeetingController

---
+++ Virtual-Meeting-Management/Virtual-Meeting-Management/Controllers/MeetingController.cs
@@ -0,0 +1,37 @@
+﻿using Application.DTOs.StreamingDto;
+using Application.Interfaces;
+using Domain.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Virtual_Meeting_Management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MeetingController : ControllerBase
+    {
+        private readonly IUseCase<CreateMeetingDto, Result> _createMeetingUseCase;
+
+        public MeetingController(IUseCase<CreateMeetingDto, Result> createMeetingUseCase)
+        {
+            _createMeetingUseCase = createMeetingUseCase;
+        }
+
+        [HttpPost("CreateMeeting")]
+        public async Task<IActionResult> CreateMeetingAsync([FromBody] CreateMeetingDto createMeetingDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _createMeetingUseCase.ExecuteAsync(createMeetingDto);
+
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
