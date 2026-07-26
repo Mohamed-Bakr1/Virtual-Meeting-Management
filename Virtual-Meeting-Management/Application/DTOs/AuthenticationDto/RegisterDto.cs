0a1149228c6c150a491696b19995f6cc96651bb7
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-26 21:44:59 +0300

    Update RegisterDto namespace for better organization
    Changed RegisterDto.cs namespace from Application.DTOs to Application.DTOs.AuthenticationDto to improve code organization and clarify its relation to authentication functionality.

--- Virtual-Meeting-Management/Application/DTOs/AuthenticationDto/RegisterDto.cs
+++ Virtual-Meeting-Management/Application/DTOs/AuthenticationDto/RegisterDto.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace Application.DTOs
+namespace Application.DTOs.AuthenticationDto
 {
     public class RegisterDto
     {
