87efe9bb560314f6ccfc3facf52034b34319a0e7
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-26 21:56:26 +0300

    Create UserContextService

---
+++ Virtual-Meeting-Management/Virtual-Meeting-Management/Web/UserContextService.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces;
+using System.Security.Claims;
+
+namespace Virtual_Meeting_Management.Web
+{
+    public class UserContextService : IUserContextService
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public UserContextService(IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext;
+        }
+        public string GetUserID()
+        {
+            var user = _httpContext.HttpContext?.User;
+            return user?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+
+        public string GetUserRole()
+        {
+            var user = _httpContext.HttpContext?.User;
+            return user?.FindFirst(ClaimTypes.Role)?.Value;
+
+        }
+    }
+}
