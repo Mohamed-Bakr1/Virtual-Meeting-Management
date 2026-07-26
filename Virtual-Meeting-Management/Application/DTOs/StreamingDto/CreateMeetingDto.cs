195b4102b8fc22c0c9e072820e7de1ed8c75e145
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-26 21:57:24 +0300

    Create CreateMeetingDto

---
+++ Virtual-Meeting-Management/Application/DTOs/StreamingDto/CreateMeetingDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.StreamingDto
+{
+    public class CreateMeetingDto
+    {
+        [Required(ErrorMessage = "[0] Field is Required!")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "[0] Field is Required!")]
+        public DateTime StartTime { get; set; }
+
+        [Required(ErrorMessage = "[0] Field is Required!")]
+        public DateTime EndTime { get; set; }
+
+        [MaxLength(500, ErrorMessage = "The Max Length is [1]!")]
+        public string? Description { get; set; }
+    }
+}
