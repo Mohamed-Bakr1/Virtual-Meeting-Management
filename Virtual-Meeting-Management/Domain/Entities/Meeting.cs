a61cb3beff1f0370041aaeb6dd0caccf923d3be2
Mohamed-Bakr1 <cabtinmohamed32@gmail.com>
2026-07-26 21:59:17 +0300

    Make Meeting.Description property nullable
    Changed the Description property in the Meeting class from string to string? to allow meetings without a description. This improves flexibility by permitting null values for Description.

--- Virtual-Meeting-Management/Domain/Entities/Meeting.cs
+++ Virtual-Meeting-Management/Domain/Entities/Meeting.cs
@@ -7,7 +7,7 @@
         public string HostId { get; set; }
 
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
     }
